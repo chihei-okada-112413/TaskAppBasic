a28faef2975fbb833eee09d6fc55a59407bece8c
EC2 Default User <ec2-user@ip-172-31-20-60.ap-northeast-1.compute.internal>
2020-09-08 04:01:53 +0000

    C8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,35 @@
 class TasksController < ApplicationController
+  before_action :set_user
+  before_action :set_task, only: [:show, :edit, :update]
+  
   def index
     @tasks=Task.all
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+    @task.task_name=params[:task][:task_name]
+    @task.detail=params[:task][:detail]
+    @task.save
+    redirect_to user_tasks_url(@user, @task)
+  end
+  def new
+  end
+  def create
+    @task=Task.new(task_name: params[:task_name], detail: params[:detail])
+    @task.save
+    redirect_to user_tasks_url
+  end
+  private
+  def set_user
+      @user = User.find(params[:user_id])
+  end
+  def set_task
+      @task=Task.find(params[:id])
+  end
+  def task_params
+      params.require(:task).permit(:task_name, :detail)
   end
 end
